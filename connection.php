77a8480d34a6654654ac4ad84ea46f19d5cff0b9
DasSuperhirn <DasSuperhirn@gmx.de>
2021-03-22 11:29:51 +0100

    a

--- connection.php
+++ connection.php
@@ -5,6 +5,8 @@ $dbuser = "root";
 $dbpass = "";
 $dbname = "u168783728_users";
 
+echo $dbhost, $dbuser, $dbpass, $dbname
+
 if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
 {
     die("failed to connect");
