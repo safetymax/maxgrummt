2acd669f9d7dfe04949589757e04cde0777b3da3
DasSuperhirn <DasSuperhirn@gmx.de>
2021-03-22 16:26:17 +0100

    a

--- maxtest.php
+++ maxtest.php
@@ -15,9 +15,10 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Max' Labor</title>
 </head>
 <body>
-    
+    <h1>Test</h1>
+    <script href="js/maxtest.js"></script>
 </body>
 </html>
