db638786b0132b0bb0ae5c4481abbb9935d97c46
DasSuperhirn <DasSuperhirn@gmx.de>
2021-03-22 11:40:28 +0100

    a

--- login.php
+++ login.php
@@ -29,9 +29,6 @@
                         header("Location: index.php");
                         die;
                    }
-                   else{
-                       echo "wrong password"
-                   }
                 }
             }
 
