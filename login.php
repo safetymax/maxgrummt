60c1360dc7eb667083f59689baa9c477b2c14ddb
DasSuperhirn <DasSuperhirn@gmx.de>
2021-03-22 11:54:38 +0100

    a

--- login.php
+++ login.php
@@ -22,7 +22,7 @@
                 if($result && mysqli_num_rows($result) > 0)
                 {
                    $user_data = mysqli_fetch_assoc($result);
-                   $dbpass = $user_data['password'];
+                   $dbpass = password_verify($password,$user_data['password']);
 
                    if(password_verify($password,$user_data['password']))
                    {
