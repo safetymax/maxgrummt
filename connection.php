5f3cf30e0ed7cb017ee112d7e93c768f6a0492a6
DasSuperhirn <DasSuperhirn@gmx.de>
2021-03-22 11:28:56 +0100

    a

--- connection.php
+++ connection.php
@@ -3,7 +3,7 @@
 $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "";
-$dbname = "login_sample_db";
+$dbname = "u168783728_users";
 
 if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
 {
