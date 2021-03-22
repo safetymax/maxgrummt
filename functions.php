e4b0eb0a721b57d2f481479d30bab65925e70b96
DasSuperhirn <DasSuperhirn@gmx.de>
2021-03-22 16:10:52 +0100

    a

--- functions.php
+++ functions.php
@@ -19,6 +19,27 @@ function check_login($con)
     die;
 }
 
+function check_login($con)
+{
+    if(isset($_SESSION['user_id']))
+    {
+        if($_SESSION['user_name']=="SafetyMax"){
+        $id = $_SESSION['user_id'];
+        $query = "select * from users where user_id = '$id' limit 1";
+        $result = mysqli_query($con,$query);
+        if($result && mysqli_num_rows($result) > 0)
+        {
+            $user_data = mysqli_fetch_assoc($result);
+            return $user_data;
+        }
+        }
+    }
+
+    //redirect to login
+    header("Location: login.php");
+    die;
+}
+
 function random_num($length)
 {
     $text = "";
