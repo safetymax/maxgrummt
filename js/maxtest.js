9fee8e4f9d792a2cd6ab40bf9e725378ea589a52
DasSuperhirn <DasSuperhirn@gmx.de>
2021-03-22 16:27:46 +0100

    a

--- js/maxtest.js
+++ js/maxtest.js
@@ -2,6 +2,8 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+console.log("test");
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
