5459381d232081b59f12e5f8047c3503b06b0d9f
DasSuperhirn <DasSuperhirn@gmx.de>
2021-03-23 12:17:41 +0100

    a

--- functions.php
+++ functions.php
@@ -79,7 +79,7 @@ function get_messages($con, $amount){
 
 function send_message($con, $user_data, $msg){
     $user_id = $user_data['user_id'];
-    $query = "insert into messages (outgoing_msg_id, msg) values ('$user_id','$msg')"
+    $query = "insert into messages (outgoing_msg_id, msg) values ('$user_id','$msg')";
     mysqli_query($con, $query);
     die;
 }
