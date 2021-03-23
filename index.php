8f6ba801084d9fcb514dd5158a20148d26e8574c
DasSuperhirn <DasSuperhirn@gmx.de>
2021-03-23 15:39:59 +0100

    a

--- index.php
+++ index.php
@@ -171,6 +171,7 @@ session_start();
             document.location.href = "chat.php";
         }
 
+        console.log(<?php echo isset($_SESSION['user_id']);?>);
         if(<?php echo isset($_SESSION['user_id']);?> == true){
             var newElement = document.createElement("button");
             newElement.innerHTML = "Logout";
