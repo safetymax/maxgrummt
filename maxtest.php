a43c52bc8509b6b1c00b0d3501609645f55941fb
DasSuperhirn <DasSuperhirn@gmx.de>
2021-03-23 11:10:11 +0100

    a

--- maxtest.php
+++ maxtest.php
@@ -22,6 +22,6 @@ session_start();
 <body>
     <h1>Test</h1>
     <script src="js/maxtest.js"></script>
-    <p><?php echo $message_data['msg']; ?>;</p>
+    <p><?php echo $message_data['id']; ?></p>
 </body>
 </html>
