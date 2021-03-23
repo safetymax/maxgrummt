397f389fd8de5c2ebd9f886241ed88955f51e791
DasSuperhirn <DasSuperhirn@gmx.de>
2021-03-23 13:37:21 +0100

    a

--- signup.php
+++ signup.php
@@ -14,8 +14,9 @@ session_start();
         {
             $test_name_query = "select user_name from users where user_name = '$user_name'";
             $test_name_result = mysqli_query($test_name_query);
-            if($test_name_result&&mysqli_num_rows($test_name_query)>0){
+            if($test_name_result && mysqli_num_rows($test_name_query)>0){
                 echo "USERNAME ALREADY TAKEN";
+                header("Location: signup.php");
                 die;
             }
             else{
