90b8bb2983bc110cf168a638ced07cf30856a9a9
DasSuperhirn <DasSuperhirn@gmx.de>
2021-03-23 15:06:19 +0100

    a

--- functions.php
+++ functions.php
@@ -80,11 +80,11 @@ function get_messages($con, $amount){
 
 function send_message($con, $user_data, $msg){
     if($msg){
-    $user_id = $user_data['user_id'];
-    $query = "insert into messages (outgoing_msg_id, msg) values ('$user_id','$msg')";
-    mysqli_query($con, $query);
+        $user_id = $user_data['user_id'];
+        $query = "insert into messages (outgoing_msg_id, msg) values ('$user_id','$msg')";
+        mysqli_query($con, $query);
+    }
     header("Location: maxtest.php");
     return;
-    }
     die;
 }
