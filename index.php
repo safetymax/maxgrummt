e7e5812eecde58cdf5edbaec0dfd4fecda98f146
DasSuperhirn <DasSuperhirn@gmx.de>
2021-03-23 20:38:48 +0100

    a

--- index.php
+++ index.php
@@ -151,7 +151,7 @@ session_start();
     <script src="js/index.js"></script> 
     <div class="div1">
     <button class="header" onclick="myclick()">Maximilian Grummt</button>
-    <p class="text">CHAT OUT NOW</p>
+    <p class="text" id="subtitle">CHAT OUT NOW</p>
     </div>
     <button class="about" onclick="aboutclick()">About</button>
     <button class="blog" onclick="blogclick()">Blog</button>
@@ -203,7 +203,29 @@ session_start();
             createPlane();
         }
 
+        //COUNTDOWN TIMER
 
+        var countDownDate = new Date("Mar 29, 2021 23:00:00").getTime();
+
+        var x = setInterval(function() {
+
+        var now = new Date().getTime();
+
+        var distance = countDownDate - now;
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("subtitle").innerHTML = "NEW UPDATE OUT NOW";
+        }
+        }, 1000);
     </script>
 </body>
 </html>
