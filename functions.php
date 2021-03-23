3d5b8f0cc5875f15acfda5f47f752c3e410e9de3
DasSuperhirn <DasSuperhirn@gmx.de>
2021-03-23 12:24:55 +0100

    a

--- functions.php
+++ functions.php
@@ -65,13 +65,15 @@ function get_messages($con, $amount){
         if($result && mysqli_num_rows($result) > 0){
             $message_data = mysqli_fetch_assoc($result);
         }
+        $counter = 0;
         for($i = mysqli_num_rows($result);$i>mysqli_num_rows($result)-$amount;$i-=1){
             $new_query = "select * from users a join messages b on a.user_id = b.outgoing_msg_id where b.id = '$i' limit 1";
             $new_result = mysqli_query($con, $new_query);
 
             echo $i;
             $new_message_data = mysqli_fetch_assoc($new_result);
-            $output[$amount-$i] = $new_message_data;
+            $output[$counter] = $new_message_data;
+            $counter++;
         }
         return $output;
         die;
