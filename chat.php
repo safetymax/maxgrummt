af6a96cb12282a052c714228d0bfdad47461ac05
DasSuperhirn <DasSuperhirn@gmx.de>
2021-03-23 16:49:05 +0100

    a

--- chat.php
+++ chat.php
@@ -22,7 +22,7 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Max' Labor</title>
+    <title>Chat</title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Questrial&display=swap" rel="stylesheet">
     <style>
