cf3df5eac990ac51933e9b3a11d496a9b1ad9747
DasSuperhirn <DasSuperhirn@gmx.de>
2021-09-19 12:53:25 +0200

    a

--- sites/connection.php
+++ sites/connection.php
@@ -5,10 +5,6 @@ $dbuser = "root";
 $dbpass = "";
 $dbname = "maximil2_accounts";
 
-if($con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
-{
-    die("website under construction but kinda works lmao");
-}
 if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
 {
     die("website under construction");
