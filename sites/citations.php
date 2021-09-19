c50fb6d5ab2ba884e67453c827d9be040a445967
DasSuperhirn <DasSuperhirn@gmx.de>
2021-09-19 15:14:01 +0200

    a

--- sites/citations.php
+++ sites/citations.php
@@ -78,10 +78,8 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
     <input class="textinput" type="text" name="message"><br><br>
     <input class="button" type="submit" value="Send">
 </form>
-<p class="text"><?php 
-for($i = 1;$i<count($message_data);$i+=1){
-    echo $message_data[$i];
-}
-?></p>
+<script>
+console.log(<?php for($i = 1;$i<count($message_data);$i+=1){echo $message_data[$i];}?>);
+</script>
 </body>
 </html>
