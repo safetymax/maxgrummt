0d4f52b94b286a25e9035331d7c5008c091d5206
DasSuperhirn <DasSuperhirn@gmx.de>
2021-09-25 14:04:09 +0200

    a

--- sites/maxtest.php
+++ sites/maxtest.php
@@ -45,8 +45,8 @@ session_start();
             display: flex;
             justify-content: center;
             align-items: center;
-            height: 10%;
-            width: 10%;
+            height: 7%;
+            width: 7%;
         }
 
         .logo {
