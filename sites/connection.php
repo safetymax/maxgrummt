1d1469c892bde498a21efd8b1e4c358a93a4cc43
DasSuperhirn <DasSuperhirn@gmx.de>
2021-11-23 12:53:32 +0100

    a

--- sites/connection.php
+++ sites/connection.php
@@ -2,7 +2,7 @@
 
 $dbhost = "localhost:3306";
 $dbuser = "maximil2_user";
-$dbpass = "MaxGrummt05!";
+$dbpass = "SQLPasswort123!";
 $dbname = "maximil2_accounts";
 
 if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
