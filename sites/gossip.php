baedebbe080540552d3c05dc3e37fd73fcccd2bb
DasSuperhirn <DasSuperhirn@gmx.de>
2021-11-25 21:23:45 +0100

    a

--- sites/gossip.php
+++ sites/gossip.php
@@ -20,6 +20,9 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
 
 <head>
     <title>HGV Gossip Box 0.1</title>
+    <style>
+        @import url(base.css);
+    </style>
 </head>
 
 <body>
@@ -28,7 +31,18 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
         <input class="textinput" type="text" name="message" placeholder="Nachricht" autocomplete="off"><br><br>
         <input class="button" type="submit" value="Add">
     </form>
+    <section class="basic-grid">
+        <?php
+            for($i = 0;$i<1;$i++){
+                echo "<div class=\"widget\"><h3>";
+                echo "{$gossip_data[$i]["title"]}";
+                echo "</h3><p>";
+                echo "{$gossip_data[$i]["msg"]}";
+                echo "</p></div>";
 
+            }
+        ?>
+    </section>
     <script>
         console.log("<?php echo "{$gossip_data[0]["title"]}"; ?>");
         console.log("<?php echo "{$gossip_data[0]["msg"]}"; ?>");
